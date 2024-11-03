8e0ee50a4111ec12705c71aaee243fa7a6ef0c19
Param <purohit.param91@gmail.com>
2024-11-03 18:45:18 +0000

    e.preventDefalt added

--- js/script.js
+++ js/script.js
@@ -77,3 +77,37 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("One or more video/image elements not found.");
     }
 });
+function dragElement(element) {
+    if (!element) {
+        console.error("Drag element is null.");
+        return;
+    }
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    element.addEventListener("mousedown", function(e) {
+        if (e.target.tagName === "INPUT" || e.target.tagName === "BUTTON") {
+            return; // Do nothing if clicking inside input or button
+        }
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    });
+
+    function elementDrag(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
